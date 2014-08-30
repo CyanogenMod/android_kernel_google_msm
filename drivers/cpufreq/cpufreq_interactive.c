392319a68d685452830b02a7ff92d84ea14666f9
Junjie Wu <junjiew@codeaurora.org>
2014-08-29 18:55:45 -0700

    cpufreq: interactive: Make window alignment optional
    Make sampling window alignment optional when scheduler inputs
    are not enabled.
    
    Change-Id: If69c111a3efe219cdd1e38c1f46f03404789c0bb
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -123,6 +123,13 @@ static int timer_slack_val = DEFAULT_TIMER_SLACK;
 static bool io_is_busy;
 
 /*
+ * Whether to align timer windows across all CPUs. When
+ * use_sched_load is true, this flag is ignored and windows
+ * will always be aligned.
+ */
+static bool align_windows = true;
+
+/*
  * Stay at max freq for at least max_freq_hysteresis before dropping frequency.
  */
 static unsigned long max_freq_hysteresis = 0;
@@ -131,9 +138,16 @@ static unsigned long max_freq_hysteresis = 0;
 static u64 round_to_nw_start(u64 jif)
 {
 	unsigned long step = usecs_to_jiffies(timer_rate);
+	u64 ret;
 
-	do_div(jif, step);
-	return (jif + 1) * step;
+	if (align_windows) {
+		do_div(jif, step);
+		ret = (jif + 1) * step;
+	} else {
+		ret = jiffies + usecs_to_jiffies(timer_rate);
+	}
+
+	return ret;
 }
 
 static void cpufreq_interactive_timer_resched(unsigned long cpu,
@@ -1039,6 +1053,28 @@ static ssize_t store_max_freq_hysteresis(struct kobject *kobj,
 static struct global_attr max_freq_hysteresis_attr = __ATTR(max_freq_hysteresis, 0644,
 		show_max_freq_hysteresis, store_max_freq_hysteresis);
 
+static ssize_t show_align_windows(struct kobject *kobj,
+				     struct attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", align_windows);
+}
+
+static ssize_t store_align_windows(struct kobject *kobj,
+			struct attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned long val;
+
+	ret = kstrtoul(buf, 0, &val);
+	if (ret < 0)
+		return ret;
+	align_windows = val;
+	return count;
+}
+
+static struct global_attr align_windows_attr = __ATTR(align_windows, 0644,
+		show_align_windows, store_align_windows);
+
 
 static struct attribute *interactive_attributes[] = {
 	&target_loads_attr.attr,
@@ -1053,6 +1089,7 @@ static struct attribute *interactive_attributes[] = {
 	&boostpulse_duration.attr,
 	&io_is_busy_attr.attr,
 	&max_freq_hysteresis_attr.attr,
+	&align_windows_attr.attr,
 	NULL,
 };
 
